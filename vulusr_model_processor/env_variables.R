fea631e4e9352c627b551eb65d187f5ce242168d
Bikingman <daniel.patterson87@gmail.com>
2021-08-20 00:29:11 -0400

    Update env_variables.R

--- vulusr_model_processor/env_variables.R
+++ vulusr_model_processor/env_variables.R
@@ -1,12 +1,12 @@
 
-Sys.setenv(SSPF_EMAIL_PASSWORD="$AcswE0EZW&41PhY")
+Sys.setenv(SSPF_EMAIL_PASSWORD="")
 
-Sys.setenv(SSPF_EMAIL_ADDRESS="mailbox@saferstreetspriorityfinder.com")
+Sys.setenv(SSPF_EMAIL_ADDRESS="")
 
-Sys.setenv(SSPF_AMAZON_HOST_ADDRESS="usdot-vulusr-t3small.crrjyzlytxji.us-east-1.rds.amazonaws.com")
+Sys.setenv(SSPF_AMAZON_HOST_ADDRESS="")
 
-Sys.setenv(SSPF_AMAZON_PASSWORD="usdot_vulusr2021")
+Sys.setenv(SSPF_AMAZON_PASSWORD="")
 
-Sys.setenv(SSPF_AMAZON_USERNAME="postgres")
+Sys.setenv(SSPF_AMAZON_USERNAME="")
 
-Sys.setenv(SSPF_AMAZON_DATABASE="a0137")
+Sys.setenv(SSPF_AMAZON_DATABASE="")
