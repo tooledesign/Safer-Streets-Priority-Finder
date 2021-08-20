f10279be6dfc1edfbffc73503d7cda0feb48cedd
brendanbikes <brendan.alan.murphy@gmail.com>
2021-08-20 14:01:56 -0500

    Switch to using configparser file

--- vulusr_model_processor/python/dockerManager.py
+++ vulusr_model_processor/python/dockerManager.py
@@ -1,10 +1,14 @@
 import psycopg2
 import subprocess
+from configparser import ConfigParser
 from time import sleep, time
 
 #This script checks to see if the docker container is fresh or not, and restarts it if not fresh.
 
-conn = psycopg2.connect(host="usdot-vulusr-t3small.crrjyzlytxji.us-east-1.rds.amazonaws.com", dbname="a0137", user="postgres", password="usdot_vulusr2021")
+config = ConfigParser()
+config.read('.sspf_config')
+
+conn = psycopg2.connect(host=config.get('postgres','host'), dbname=config.get('postres','dbname'), user=config.get('postgres','user'), password=config.get('postgres','password'))
 
 def checkDockerStatus():
     with conn.cursor() as cur:
@@ -25,15 +29,15 @@ def process():
     isFresh = checkDockerStatus()
     if isFresh == False:
         #stop docker container
-        runCommand('sudo docker stop beta_processor')
+        runCommand('sudo docker stop sspf_model_processor')
         #prune docker
         runCommand('sudo docker container prune -f')
         #get current timestamp
         ts = time()
         #initialize new container with a mounted volume to save any logs/data in
-        runCommand('sudo docker run -d -v /var/run/docker.sock:/var/run/docker.sock -v beta_processor_{}:/logs --net sp-example-net --name=beta_processor --restart=always -p 81:3839 vulusr_beta_processor_v1'.format(ts))
+        runCommand('sudo docker run -d -v /var/run/docker.sock:/var/run/docker.sock -v sspf_model_processor_{}:/logs --net sp-example-net --name=sspf_model_processor --restart=always -p 9001:3839 sspf_model_processor'.format(ts))
         #file the logs according to current timestamp -- use the volume location created by docker
-        runCommand('sudo docker logs -f beta_processor &> /var/lib/docker/volumes/beta_processor_{0}/beta_processor_{0}.log &'.format(ts))
+        runCommand('sudo docker logs -f sspf_model_processor &> /var/lib/docker/volumes/sspf_model_processor_{0}/sspf_model_processor_{0}.log &'.format(ts))
         sleep(5)
         setDockerStatus()
 
--- vulusr_model_processor/python/userDataCleanup.py
+++ vulusr_model_processor/python/userDataCleanup.py
@@ -1,9 +1,13 @@
 import psycopg2
+from configparser import ConfigParser
 from time import time
 
 #This script checks for 6-month stale user data for specific model runs and deletes it if user opted out of data collection.
 
-conn = psycopg2.connect(host="usdot-vulusr-t3small.crrjyzlytxji.us-east-1.rds.amazonaws.com", dbname="a0137", user="postgres", password="usdot_vulusr2021")
+config = ConfigParser()
+config.read('.sspf_config')
+
+conn = psycopg2.connect(host=config.get('postgres','host'), dbname=config.get('postres','dbname'), user=config.get('postgres','user'), password=config.get('postgres','password'))
 
 def cleanupStaleRuns(type, threshold=6):
     #checks 3 storage types independently
@@ -21,7 +25,7 @@ def cleanupStaleRuns(type, threshold=6):
 
 def deleteStaleRun(user_id, run_id, type):
     with conn.cursor() as cur:
-        #cur.execute("DROP TABLE IF EXISTS local_user_data.{}_{}_{};".format('crashes' if type == 'crash' else type, user_id, run_id))
+        cur.execute("DROP TABLE IF EXISTS local_user_data.{}_{}_{};".format('crashes' if type == 'crash' else type, user_id, run_id))
         cur.execute("UPDATE gen_management.accounts SET date_{}_deleted = current_timestamp(0) WHERE user_id = %s AND run_id = %s;".format(type), (user_id, run_id))
         conn.commit()
 
