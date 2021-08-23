4f24644c5024ed5cf264c8894860a3b288a546b7
Bikingman <daniel.patterson87@gmail.com>
2021-08-22 21:19:15 -0400

    reposition reporter to a different container, provide messaging to user on status

--- safer_streets_priority_finder/R/fct_db_utils.R
+++ safer_streets_priority_finder/R/fct_db_utils.R
@@ -1150,3 +1150,22 @@ get_columns <- function(
     Add_Modal(title = 'Something Went Wrong.', body=c)
   })
 }
+
+
+fetch_report_from_s3 = function(bucket=Sys.getenv("S3_BUCKET"), user_id, run_id) {
+  tryCatch({
+    print(glue::glue('Retrieving {user_id}_{run_id}_report.pdf from S3 bucket'))
+    if (aws.s3::object_exists(glue::glue('{user_id}_{run_id}_report.pdf'), bucket)) {
+      #file <- aws.s3::get_object(glue::glue('{user_id}_{run_id}_report.pdf'), bucket)
+      #return(file)
+      # or save object to disc
+      aws.s3::save_object(glue::glue('{user_id}_{run_id}_report.pdf'), bucket, file=glue::glue('{user_id}_{run_id}_report.pdf'))
+    } else {
+      print(glue::glue('{user_id}_{run_id}_report.pdf not found'))
+      return(NULL)
+    }
+  }, error = function(cond){
+    print('There was an error retreiving report.')
+    print(cond)
+  })
+}
