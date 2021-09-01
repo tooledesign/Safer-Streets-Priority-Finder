25c5daeff913dc560a59f5e3739b4a8777417494
Bikingman <daniel.patterson87@gmail.com>
2021-09-01 13:08:12 -0400

    encrypt passwords

--- safer_streets_priority_finder/R/fct_login_funcs.R
+++ safer_streets_priority_finder/R/fct_login_funcs.R
@@ -4,10 +4,11 @@ test_for_run <- function(connection, username, run_id){
   tryCatch({
     username <- DBI::dbQuoteString(connection,username)
     run_id <- DBI::dbQuoteString(connection,run_id)
-    key <- DBI::dbQuoteString(connection, Sys.getenv("USER_DATA_KEY"))
-    q <- glue::glue("SELECT (EXISTS (SELECT FROM gen_management.accounts WHERE pgp_sym_decrypt(decode(username, 'hex'), {key}) = {username} AND run_id = {run_id} ))::INTEGER;")
+    
+    q <- glue::glue("SELECT (EXISTS (SELECT FROM gen_management.accounts WHERE username = {username} AND run_id = {run_id} ))::INTEGER;")
+
     exists <- DBI::dbGetQuery(connection, q)[1,1] 
-    if (exists  == 0 ) {
+    if (exists == 0 ) {
       return('FALSE')
     } else if (exists == 1) {
       return('TRUE')
@@ -26,21 +27,20 @@ get_user_id <- function(connection, username, password){
   tryCatch({
         username <-  DBI::dbQuoteString(connection, username)
         key <- DBI::dbQuoteString(connection, Sys.getenv("USER_DATA_KEY"))
+        pepper <- DBI::dbQuoteString(connection, Sys.getenv("PEPPER"))
+        password <-  DBI::dbQuoteString(connection, password)
         
         # get salt 
-        salt <- DBI::dbGetQuery(connection, glue::glue("SELECT salt FROM gen_management.salt WHERE pgp_sym_decrypt(decode(username, 'hex'), {key}) = {username};"))[1,1] 
+        salt <- as.character(DBI::dbGetQuery(connection, glue::glue("SELECT salt FROM gen_management.salt WHERE pgp_sym_decrypt(decode(username, 'hex'), {key}) = {username};"))[1,1]) 
+        sterilized_salt <-  DBI::dbQuoteString(connection, salt)
         
-        # hash pw 
-        hashed_pw <- sodium::bin2hex(sodium::sha256(charToRaw(password)))
-        password <- DBI::dbQuoteString(connection, paste0(hashed_pw, salt,  Sys.getenv("PEPPER")))
+        q <- glue::glue("SELECT DISTINCT user_id 
+                          FROM gen_management.accounts 
+                          WHERE username = {username}
+                          AND pgp_sym_decrypt(decode(password, 'hex'), {key}) = CONCAT({password}, {sterilized_salt}, {pepper});")
+        user_id <- DBI::dbGetQuery(connection, q)
 
-        user_id <- DBI::dbGetQuery(connection, glue::glue("SELECT DISTINCT user_id 
-                                                          FROM gen_management.accounts 
-                                                          WHERE pgp_sym_decrypt(decode(username, 'hex'), {key}) = {username} 
-                                                          AND password = {password};"))
-                                                          
-
-        if (is.null(toString(user_id)) || is.na(toString(user_id)) || toString(user_id) == ""){
+        if (is.null(toString(user_id)) || is.na(toString(user_id)) || toString(user_id) == "" || nrow(user_id) == 0){
           return(-999)
         }  else {
           return(toString(user_id)) 
@@ -55,16 +55,14 @@ get_user_id <- function(connection, username, password){
 # returns study id associated with login credentials 
 get_run_id <- function(connection, username, run_id){
   tryCatch({
-  
+
       username <-  DBI::dbQuoteString(connection, username)
       run_id <-  DBI::dbQuoteLiteral(connection, run_id)
-      key <- DBI::dbQuoteString(connection, Sys.getenv("USER_DATA_KEY"))
-
       run_id <- DBI::dbGetQuery(connection, glue::glue("SELECT DISTINCT run_id 
                                                         FROM gen_management.accounts 
-                                                        WHERE pgp_sym_decrypt(decode(username, 'hex'), {key}) = {username} 
+                                                        WHERE username = {username}
                                                         AND run_id = {run_id};"))
-      
+
       # test for duplicate ids 
       if (length(run_id) == 1){
         return(toString(run_id))
@@ -88,9 +86,9 @@ get_email_address <- function(connection, username){
       key <- DBI::dbQuoteString(connection, Sys.getenv("USER_DATA_KEY"))
 
       email <- NULL
-      q <- glue::glue("SELECT DISTINCT pgp_sym_decrypt(decode(email, 'hex'), {key})
+      q <- glue::glue("SELECT DISTINCT email
                        FROM gen_management.accounts 
-                       WHERE pgp_sym_decrypt(decode(username, 'hex'), {key}) = {username};")
+                       WHERE username = {username};")
       email <- DBI::dbGetQuery(connection, q)
       
       if (is.null(email)) {
@@ -112,8 +110,9 @@ add_user <- function(connection, username, run_id, email, password, original_use
   tryCatch({
     username <- DBI::dbQuoteString(connection, username)
     key <- DBI::dbQuoteString(connection, Sys.getenv("USER_DATA_KEY"))
-    
-    q <- glue::glue("SELECT (EXISTS (SELECT FROM gen_management.accounts WHERE pgp_sym_decrypt(decode(username, 'hex'), {key}) = {username}))::INTEGER;")
+
+    q <- glue::glue("SELECT (EXISTS (SELECT FROM gen_management.accounts WHERE username = {username}))::INTEGER;")
+
     exists <- DBI::dbGetQuery(connection, q)[1,1] 
     if (exists == 1){
       print('This username already exists, please choose something else.')
@@ -123,24 +122,25 @@ add_user <- function(connection, username, run_id, email, password, original_use
       # salt
       salt <- sodium::bin2hex(sodium::random(8))
       sterilized_salt <- DBI::dbQuoteString(connection, salt)
-
+      pepper <- DBI::dbQuoteString(connection, Sys.getenv("PEPPER"))
       # add salt for future use 
       q <- glue::glue("INSERT INTO gen_management.salt (username, salt) VALUES (encode(pgp_sym_encrypt({username}, {key}), 'hex'), {sterilized_salt});")
       DBI::dbGetQuery(connection, q)
-      
-      # bind password and salt 
-      password <- DBI::dbQuoteString(connection, paste0(sodium::bin2hex(sodium::sha256(charToRaw(password))), salt, Sys.getenv("PEPPER")))
-
 
       # sterilize values
       run_id <- DBI::dbQuoteString(connection, run_id)
+      password <- DBI::dbQuoteString(connection, password)
       email <- DBI::dbQuoteString(connection, email)
       original_username <- DBI::dbQuoteString(connection, original_username)
       original_run_id <- DBI::dbQuoteString(connection, original_run_id)
 
-      q <- glue::glue("INSERT INTO gen_management.accounts ( username, password, user_id, run_id, email, o_username, o_run_id) VALUES (encode(pgp_sym_encrypt({username}, {key}), 'hex'), {password}, EXTRACT(EPOCH FROM NOW()), {run_id}, encode(pgp_sym_encrypt({email}, {key}), 'hex'), encode(pgp_sym_encrypt({original_username}, {key}), 'hex'), {original_run_id});")
+      #insert username 
+      q <- glue::glue("INSERT INTO gen_management.accounts ( username, password, user_id, run_id, email, o_username, o_run_id) 
+                       VALUES ({username}, encode(pgp_sym_encrypt(CONCAT({password}, {sterilized_salt}, {pepper}), {key}), 'hex'), EXTRACT(EPOCH FROM NOW()), {run_id}, {email}, {original_username}, {original_run_id});")
       DBI::dbGetQuery(connection, q)
+      
       print(glue::glue('Added username: {username}'))
+      
       return('User added')
     } else {
       return('Error occured')
@@ -158,19 +158,25 @@ add_run <- function (connection,  username, run_id, password, original_run_id ){
   tryCatch({
     username <- DBI::dbQuoteString(connection,username)
     run_id <- DBI::dbQuoteString(connection,run_id)
-    password<-DBI::dbQuoteString(connection, password)
-    email <- get_email_address(connection=connection, username = username)
+    password <- DBI::dbQuoteString(connection,password)
+    email <- DBI::dbQuoteString(connection, get_email_address(connection=connection, username = username))
     email <- DBI::dbQuoteString(connection, email)
     key <- DBI::dbQuoteString(connection, Sys.getenv("USER_DATA_KEY"))
-
-    q <- glue::glue("SELECT DISTINCT pgp_sym_decrypt(decode(o_username, 'hex'), {key})
+    pepper <- DBI::dbQuoteString(connection, Sys.getenv("PEPPER"))
+    q <- glue::glue("SELECT DISTINCT o_username
                         FROM gen_management.accounts 
-                        WHERE pgp_sym_decrypt(decode(username, 'hex'), {key})={username};")
-    o_un <- DBI::dbGetQuery(connection, q)[,1]
+                        WHERE username = {username};")
+    
+    salt <- as.character(DBI::dbGetQuery(connection, glue::glue("SELECT salt FROM gen_management.salt WHERE pgp_sym_decrypt(decode(username, 'hex'), {key}) = {username};"))[1,1]) 
+    sterilized_salt <-  DBI::dbQuoteString(connection, salt)
+    o_un <- DBI::dbGetQuery(connection, q)[1,1]
     original_username <- DBI::dbQuoteString(connection, o_un)
     original_run_id <- DBI::dbQuoteString(connection, original_run_id)
     user_id <- get_user_id(connection=connection, username = username, password=password)
-    q <- glue::glue("INSERT INTO gen_management.accounts ( username, user_id, password, run_id, email, o_username, o_run_id) VALUES (encode(pgp_sym_encrypt({username}, {key}), 'hex'),{user_id},{password},{run_id},encode(pgp_sym_encrypt({email}, {key}), 'hex'),encode(pgp_sym_encrypt({original_username}, {key}), 'hex'),{original_run_id});")
+    
+    #insert new run
+    q <- glue::glue("INSERT INTO gen_management.accounts ( username, user_id, password, run_id, email, o_username, o_run_id)  
+                    VALUES ({username}, {user_id}, encode(pgp_sym_encrypt(CONCAT({password}, {sterilized_salt}, {pepper}), {key}), 'hex'), {run_id}, {email}, {original_username}, {original_run_id});")
     DBI::dbGetQuery(connection, q)
   }, error = function(cond){
     c <- toString(cond)
