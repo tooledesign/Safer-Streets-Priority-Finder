14f6d57cc3273f6f9074026df0e3971a82482401
Theja Putta <pvvktheja@gmail.com>
2023-08-28 13:30:30 -0600

    sql to python

--- general/update_fars/04_backup_old_fars_data.sql
+++
@@ -1,7 +0,0 @@
--- This is run on the main SSPF database
-CREATE TABLE static.fars_processed_2015_2019_backup (LIKE static.fars_processed INCLUDING ALL);
-
-INSERT INTO static.fars_processed_2015_2019_backup
-    SELECT *
-    FROM static.fars_processed
-;
